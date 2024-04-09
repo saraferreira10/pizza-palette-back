fa51dea08a083b85ea24997b2669e24b229b08b3
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-04-09 14:23:03 -0300

    fix: put method

--- src/main/java/com/sara/pizzapalette/controller/PizzaController.java
+++ src/main/java/com/sara/pizzapalette/controller/PizzaController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,29 @@ public class PizzaController {
     @PostMapping
     public ResponseEntity<Pizza> save(@RequestBody Pizza pizza) {
         return ResponseEntity.status(HttpStatus.CREATED).body(pizzaRepository.save(pizza));
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> edit(@RequestBody Pizza pizza) {
+        try {
+            if (!pizzaRepository.findById(pizza.getId()).isPresent()) {
+                throw new EntityNotFoundException("pizza not found!");
+            }
+
+            return ResponseEntity.status(HttpStatus.OK).body(pizzaRepository.save(pizza));
+        } catch (EntityNotFoundException entityNotFoundException) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(entityNotFoundException.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable(name = "id") Long id) {
+        try {
+            pizzaRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("pizza deleted");
+        } catch (EntityNotFoundException entityNotFoundException) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(entityNotFoundException.getMessage());
+        }
     }
 
 }
